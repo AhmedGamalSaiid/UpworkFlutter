c3bd7dcfe0bca126b74f4b51ac1844125dcfed41
unknown <eamany25@gmail.com>
2021-05-01 01:05:24 +0200

    reports and proposals pagesssssss

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
-// import 'package:upwork/View/components/Talent/Inreview.dart';
-// import 'package:upwork/View/components/Talent/Pending.dart';
-//import 'package:upwork/View/Pages/TalentPages/Proposals.dart';
-// import 'package:upwork/View/Pages/BeforeLoginPages/welcome.dart';
-import 'package:upwork/View/Pages/TalentPages/Proposals.dart';
-//import 'package:upwork/View/Pages/TalentPages/reports.dart';
-// import 'package:upwork/View/components/Talent/Available.dart';
-// import 'package:upwork/View/components/Talent/Workinprogress.dart';
+import 'package:upwork/View/Pages/BeforeLoginPages/welcome.dart';
+
 import 'constanse.dart';
 
 void main() {
@@ -24,14 +18,7 @@ class MyApp extends StatelessWidget {
         primaryColor: bgUpwork,
         scaffoldBackgroundColor: Colors.white,
       ),
-      home: ProposalsPage(),
-      //ReportsPage(),
-      //AvailablePage(),
-
-      //WorkInProgressPage(),
-      //PendingPage(),
-      //InReviewPage(),
-      //Welcome(),
+      home: Welcome(),
     );
   }
 }
