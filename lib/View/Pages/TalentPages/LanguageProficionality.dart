b17a8a8b83d8f1a312dd74da0a2e66fd28621873
alaa-refat <alaa.refat1996@gmail.com>
2021-05-16 11:37:18 +0200

    language proficionality page creation

---
+++ lib/View/Pages/TalentPages/LanguageProficionality.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LanguageProficionality extends StatefulWidget {
+  @override
+  _LanguageProficionalityState createState() => _LanguageProficionalityState();
+}
+
+class _LanguageProficionalityState extends State<LanguageProficionality> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_statusbar_manager/flutter_statusbar_manager.dart';
+import 'package:upwork/View/Pages/TalentPages/LanguageProficionality.dart';
 import 'package:upwork/View/Pages/TalentPages/Messages.dart';
 
 import 'View/Pages/TalentPages/Contracts.dart';
@@ -25,21 +26,23 @@ class MyApp extends StatelessWidget {
     FlutterStatusbarManager.setColor(Colors.white, animated: true);
     var login = auth.currentUser;
     return MaterialApp(
-        title: 'Upwork',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primaryColor: bgUpworkDark,
-          scaffoldBackgroundColor: Colors.white,
-        ),
-        initialRoute: '/',
-        routes: {
-          '/': (context) => login != null ? HomePage() : Welcome(),
-          '/jobs': (context) => HomePage(),
-          '/proposals': (context) => ProposalsPage(),
-          '/contracts': (context) => Contracts(),
-          '/messages': (context) => Messages(),
-          // '/alerts': (context) => (),
-          '/reports': (context) => ReportsPage(),
-        });
+      title: 'Upwork',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: bgUpworkDark,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      // initialRoute: '/',
+      // routes: {
+      //   '/': (context) => login != null ? HomePage() : Welcome(),
+      //   '/jobs': (context) => HomePage(),
+      //   '/proposals': (context) => ProposalsPage(),
+      //   '/contracts': (context) => Contracts(),
+      //   '/messages': (context) => Messages(),
+      //   // '/alerts': (context) => (),
+      //   '/reports': (context) => ReportsPage(),
+      // }
+      home: LanguageProficionality(),
+    );
   }
 }
