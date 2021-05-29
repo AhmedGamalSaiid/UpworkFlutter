00284abedcb17c95dbb0014978c0e61804b55c51
alaa-refat <alaa.refat1996@gmail.com>
2021-05-29 21:22:36 +0200

    create client data model and service , update job data model and make job details page with dynamic data and modify the  UI of the page

--- lib/Models/JobData.dart
+++ lib/Models/JobData.dart
@@ -1,10 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class JobDataModel {
   JobDataModel({
     this.authID,
     this.freelancerNeed,
     this.jobBudget,
     this.jobCategory,
-    this.jobDecription,
+    this.jobDescription,
     this.jobDuration,
     this.jobExperienceLevel,
     this.jobImages,
@@ -12,13 +14,15 @@ class JobDataModel {
     this.jobTitle,
     this.jobType,
     this.jobVisibility,
+    this.skills,
+    this.postTime,
   });
 
   String authID;
   String freelancerNeed;
   String jobBudget;
   String jobCategory;
-  String jobDecription;
+  String jobDescription;
   String jobDuration;
   String jobExperienceLevel;
   List jobImages;
@@ -26,19 +30,22 @@ class JobDataModel {
   String jobTitle;
   String jobType;
   String jobVisibility;
+  List skills;
+  Timestamp postTime;
 
   factory JobDataModel.fromJson(Map<String, dynamic> json) => JobDataModel(
-        authID: json["authID"],
-        freelancerNeed: json["freelancerNeed"],
-        jobBudget: json["jobBudget"],
-        jobCategory: json["jobCategory"],
-        jobDecription: json["jobDecription"],
-        jobDuration: json["jobDuration"],
-        jobExperienceLevel: json["jobExperienceLevel"],
-        jobImages: json["jobImages"],
-        jobPaymentType: json["jobPaymentType"],
-        jobTitle: json["jobTitle"],
-        jobType: json["jobType"],
-        jobVisibility: json["jobVisibility"],
-      );
+      authID: json["authID"],
+      freelancerNeed: json["freelancerNeed"],
+      jobBudget: json["jobBudget"],
+      jobCategory: json["jobCategory"],
+      jobDescription: json["jobDescription"],
+      jobDuration: json["jobDuration"],
+      jobExperienceLevel: json["jobExperienceLevel"],
+      jobImages: json["jobImages"],
+      jobPaymentType: json["jobPaymentType"],
+      jobTitle: json["jobTitle"],
+      jobType: json["jobType"],
+      jobVisibility: json["jobVisibility"],
+      skills: json["skills"],
+      postTime: json["postTime"]);
 }
